61f2387224ea9af967ceedf51148f3b863d4c195
John Kopan <627905>
2018-10-17 08:12:53 -0400

    Just have some examples for me to reference for later

--- Comp Sci 2.0/Your To Do List/app4.js
+++ Comp Sci 2.0/Your To Do List/app4.js
@@ -23,18 +23,20 @@ $(document).ready(function() {
     localStorage.setItem("personData", person1);
     localStorage.getItem("personData");*/
     
-    //For loop for bullet points
-    
     
     //Get information from input box & put into data object in code (Use arrays)
     let nombre = $("input#text").val();
     
     //Pulls or retrieves data from the browser & puts into text boxes
-    
+    $("p#response").text(nombre);
     
     //Save data
-    
+    localStorage.setItem("personData", person1);
+    localStorage.getItem("personData");
     
     //Add click event listener to the button (Use a for loop here because this happens everytime the user clicks the save button)
+    
+    
+    //jQuery on the alert yes or no, if yes, add new bullet point, else (no), don't create bullet point
     
 });
