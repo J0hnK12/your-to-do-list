b0d4fae29166a4efad4877f9f8ee108170c7099a
John Kopan <627905>
2018-10-18 08:22:18 -0400

    Got to make a bullet point A

--- Comp Sci 2.0/Your To Do List/app4.js
+++ Comp Sci 2.0/Your To Do List/app4.js
@@ -1,47 +1,20 @@
 $(document).ready(function() {
-    /*//Get text from input box and remember
-    let nombre = $("input#firstname").val();
-    
-    //Put text on page
-    $("p#response").text(nombre);
-    
-    //Create new element
-    //At the bottom of the page
-    $("<p></p>").appendTo("body");
-    
-    let person1 = {
-        //Key       Value   (Pairs)
-        firstName: "John",
-        lastName: "Smith",
-        email: "jsmith@gmail.com"
-    }
-    
-    person1.lastName
-    person1["lastName"]
-    
-    //Saving to the Browser
-    localStorage.setItem("personData", person1);
-    localStorage.getItem("personData");*/
-    
     
     //Get information from input box & put into data object in code (Use arrays)
-    let nombre = $("myInput").val();
+    let nombre = $("#myInput").val();
+    $("#myBtn").click(function() {
+		$("<li><input id = 'myInput'></li>").appendTo("ul");	
+	});
     
     //Pulls or retrieves data from the browser & puts into text boxes
-    $("myInput").text(nombre);
+    $("#myInput").text(nombre);
     
     //Add click event listener to Save button
     for (var i = 0; i < 10; i++) {
-        localStorage.setItem("myInput", input1);
-        localStorage.getItem("myInput");
+        localStorage.setItem("#myInput", input1);
+        localStorage.getItem("#myInput");
     }
     
-    //jQuery on the alert "Okay" (yes) or "Cancel" (no), if the user clicks the okay button, add new bullet point. However, if the user clicks the cancel button, don't create bullet point
-    if () {
-        
-    }
-        
-    else {
-        
-    }
+    //If and else statement for the buttons in the alert saying "Okay" (yes) or "Cancel" (no), if the user clicks the okay button, add new bullet point. However, if the user clicks the cancel button, don't create bullet point
+    
 });
